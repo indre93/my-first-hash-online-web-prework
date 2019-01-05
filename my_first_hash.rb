60ef29ecd03af28895e16d3876a6adfa97b4f3d7
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-05 23:28:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,15 @@
 def my_hash
-  your_hash = {"name" => "Indre Bakeer", "age" => 25, "country of birth" => "France"}
+  your_hash = {
+    "name" => "Indre Bakeer", 
+    "age" => 25, 
+    "country of birth" => "France"
+    
+  }
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  the_manifest = {
 
 
 end
