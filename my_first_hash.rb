40b689183b681fdd2fa556ae1f4cd06bbdb15051
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-05 23:50:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,8 +32,8 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-shipping_manifest"muskets" => 2
-shipping_manifest"gun powder" => 4
+                  "muskets" => 2
+                  "gun powder" => 4
   # return the shipping_manifest hash below
                  }
 
